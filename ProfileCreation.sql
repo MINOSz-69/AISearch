fbff174092f38d9249ca7342ab29dbffbcf21a0e
Akash-SY <akashsy489@gmail.com>
2025-05-11 17:52:36 +0530

    Updated code for Profile Creation

--- ProfileCreation.sql
+++ ProfileCreation.sql
@@ -1,8 +1,31 @@
-BEGIN                                                                          
-dbms_cloud.create_credential (                                                 
-    credential_name => 'gemini_credential',                                            
-    username => 'google',                                                 
-    password => 'your-api-key-goes-here'
-);                             
-END;                                                                           
+Create a Select AI profile.
+
+begin    
+    -- Drop the profile in case it already exists
+    dbms_cloud_ai.drop_profile(
+        profile_name => 'genai',
+        force => true
+    );    
+
+    -- Create an AI profile that uses the default Gemini model
+    DBMS_CLOUD_AI.CREATE_PROFILE(
+        profile_name => 'GENAI',
+        attributes   => '{
+            "provider": "google",
+            "credential_name": "gemini_credential",
+            "comments": "true",
+            "object_list": [
+                {"owner": "ADMIN", "name": "REAL_ESTATE_LISTINGS_V13"}
+            ]
+        }'
+    );
+END;
 /
+
+Test the AI Profile
+
+SELECT DBMS_CLOUD_AI.GENERATE(
+    prompt       => 'List properties available like in Brooklyn under $300,000000 from REAL_ESTATE_LISTINGS_V13',
+    profile_name => 'GENAI',
+    action       => 'showsql')
+FROM dual;
